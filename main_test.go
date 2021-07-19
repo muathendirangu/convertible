c18336dae8caf8bb47d4d21c5bf3b688e70da762
muathendirangu <muathe.ndirangu@gmail.com>
2021-07-19 19:20:45 +0300

    add keys to the mock object passed to the body variable to fix test failure

--- main_test.go
+++ main_test.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bytes"
-	"log"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -13,20 +12,23 @@ var addConversionChoices= []struct{
 }{
   {
    
-    body: []byte(`{"NGN","KES",200}`),
+    body: []byte(`{"from":"NGN","to":"KES","amount":200}`),
     wantedCode: http.StatusOK,
   },
   {
     body: []byte("{}"),
     wantedCode: http.StatusBadRequest,
   },
+  {
+	body: []byte(`{"amount": "5","from": "KES","to": "GHC"}`),
+	wantedCode: http.StatusBadRequest,
+  },
 }
-func TestConvertCurrency(t *testing.T)  {
+func TestCurrencyConverterHandler(t *testing.T)  {
 	for _, testCase := range addConversionChoices{
 		recorder := httptest.NewRecorder()
 		request,_:=http.NewRequest(http.MethodPost, "/",bytes.NewBuffer(testCase.body))
 		currencyConverterHandler(recorder,request)
-		log.Fatal(recorder.Code)
 		if recorder.Code != testCase.wantedCode {
 			t.Errorf("got %d wanted %d", recorder.Code, testCase.wantedCode)
 		}		
