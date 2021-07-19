d3bf8b75c8a06911ef784b8c74e37bbb1f379825
muathendirangu <muathe.ndirangu@gmail.com>
2021-07-19 13:29:00 +0300

    add From,To,InitialAmount,ConvertedAmount, DefaultConversionRate to the CurrencyResponse object

--- currency/currency.go
+++ currency/currency.go
@@ -16,8 +16,11 @@ type CurrencyRequest struct{
 }
 
 type CurrencyResponse struct{
+	From,To string
+	InitialAmount,ConvertedAmount float64
+	DefaultConversionRate float64
 	Message interface{}
-	Amount float64
+	
 }
 type Rate struct{
 	CurrencyFrom string `json:"currencyfrom"`
@@ -42,12 +45,17 @@ func (e ExchangeRate) Exchange(amount float64, from,to string)(CurrencyResponse)
 	for _,v := range e.Rates{
 		if v.CurrencyFrom == from && v.CurrencyTo==to {
 			return CurrencyResponse{
-				Message: fmt.Sprintf(" Amount %v %s is equivalent to %v %s after conversion using the rate %v",amount,from,math.Round((amount*v.ConversionRate)*100)/100,to,v.ConversionRate),
-				Amount:math.Round((amount*v.ConversionRate)*100)/100,
+				From: from,
+				To: to,
+				InitialAmount: amount,
+				ConvertedAmount:math.Round((amount*v.ConversionRate)*100)/100,
+				DefaultConversionRate:v.ConversionRate,
+				Message: fmt.Sprintf(" Amount %v %s is equivalent to %v %s after conversion using the rate of 1 %v equals %v %v",amount,from,math.Round((amount*v.ConversionRate)*100)/100,to,v.CurrencyFrom,v.ConversionRate,v.CurrencyTo),
+				
 			} 
 		}
 	}
 	return CurrencyResponse{
-		Message: "something is off kindly ensure that data input is correct",
+		Message: "something is off kindly ensure that data input is correct. i.e the conversion currency choice should be uppercase e.g NGN and amount should be a number e.g 300",
 	}  
 }
