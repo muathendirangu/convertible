cbd70f406290b308fc7571302cb6b6468938c799
muathendirangu <muathe.ndirangu@gmail.com>
2021-07-19 13:25:50 +0300

    update Amount to ConvertedAmount

--- currency/currency_test.go
+++ currency/currency_test.go
@@ -25,49 +25,49 @@ func TestCurrencyExchange(t *testing.T)  {
 	t.Run("test conversion from NGN to KES", func(t *testing.T) {
 		nairaToKes :=ex.Exchange(200,"NGN","KES")
 		want := float64(52)
-		got := nairaToKes.Amount
+		got := nairaToKes.ConvertedAmount
 		assertMessage(t,got,want)
 	})
 	t.Run("test conversion from NGN to GHC", func(t *testing.T) {
 		nairaToCedi :=ex.Exchange(200,"NGN","GHC")
 		want := float64(2.8)
-		got := nairaToCedi.Amount
+		got := nairaToCedi.ConvertedAmount
 		assertMessage(t,got,want)
 	})
 	t.Run("test conversion from KES to NGN", func(t *testing.T) {
 		kesToNaira :=ex.Exchange(200,"KES","NGN")
 		want := float64(760)
-		got := kesToNaira.Amount
+		got := kesToNaira.ConvertedAmount
 		assertMessage(t,got,want)
 	})
 	t.Run("test conversion from KES to GHC", func(t *testing.T) {
 		kesToCedi :=ex.Exchange(200,"KES","GHC")
 		want := float64(11)
-		got := kesToCedi.Amount
+		got := kesToCedi.ConvertedAmount
 		assertMessage(t,got,want)
 	})
 	t.Run("test conversion from GHC to NGN", func(t *testing.T) {
 		cediToNaira :=ex.Exchange(200,"GHC","NGN")
 		want := float64(13812)
-		got := cediToNaira.Amount
+		got := cediToNaira.ConvertedAmount
 		assertMessage(t,got,want)
 	})
 	t.Run("test conversion from GHC to KES", func(t *testing.T) {
 		cediToKes :=ex.Exchange(200,"GHC","KES")
 		want := float64(3634)
-		got := cediToKes.Amount
+		got := cediToKes.ConvertedAmount
 		assertMessage(t,got,want)
 	})
 	t.Run("test passing empty first currency choice", func(t *testing.T) {
 		emptyFirstCurrency := ex.Exchange(200,"","NGN")
 		want:= float64(0)
-		got := emptyFirstCurrency.Amount
+		got := emptyFirstCurrency.ConvertedAmount
 		assertMessage(t,got,want)
 	})
 	t.Run("test passing empty second currency choice", func(t *testing.T) {
 		emptySecondCurrency := ex.Exchange(200,"NGN","")
 		want:= float64(0)
-		got := emptySecondCurrency.Amount
+		got := emptySecondCurrency.ConvertedAmount
 		assertMessage(t,got,want)
 	})
 
